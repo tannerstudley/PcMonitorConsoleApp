98cb24196eb0abc5bd003db5bac1bd041e8eb315
Tanner Studley <tannerstudley@comcast.net>
2024-06-24 10:46:13 -0400

    Writing memory percentage every 1 second.

--- PcMonitorConsoleApp/PcMonitorConsoleApp.cpp
+++ PcMonitorConsoleApp/PcMonitorConsoleApp.cpp
@@ -4,10 +4,28 @@
 #include <iostream>
 #include <windows.h>
 #include <string>
+#include <chrono>
+#include <ctime> 
 
 std::string GetMemoryPercentage();
+void PrintToConsole();
+
+auto lastSentTime = std::chrono::system_clock::now();
 
 int main()
+{
+    lastSentTime = std::chrono::system_clock::now();
+    
+    while (1) {
+        if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - lastSentTime) >= std::chrono::seconds(1))
+        {
+            PrintToConsole(); 
+            lastSentTime = std::chrono::system_clock::now(); 
+        }
+    }
+}
+
+void PrintToConsole()
 {
     std::cout << "Memory Percentage: " + GetMemoryPercentage() + "%" << std::endl;
 }
